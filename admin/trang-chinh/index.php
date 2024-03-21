126ef8444777406370167c67cc1aee3073fc0bc8
haidt1903 <dothanhhai19032004@gmail.com>
2024-03-22 05:37:34 +0700

    hang hoa

--- admin/trang-chinh/index.php
+++ admin/trang-chinh/index.php
@@ -13,7 +13,6 @@ $loai = count(loai_select_all());
 $hang_hoa = count(hang_hoa_select_all());
 $khach_hang = count(khach_hang_select_all());
 $binh_luan = count(binh_luan_select_all());
-
 $VIEW_NAME = "trang-chinh/home.php";
 
 require "../layout.php";
